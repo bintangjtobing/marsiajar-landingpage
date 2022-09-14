58ede204d5f4bc25c2527bc1715d897d8733f943
bintangjtobing <hello@bintangtobing.com>
2022-09-14 15:38:38 +0700

    update: analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,6 +80,20 @@
         gtag('config', 'G-CVJZM22LL0');
 
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-241163449-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-241163449-1');
+
+    </script>
+
 </head>
 
 <body class="stretched">
