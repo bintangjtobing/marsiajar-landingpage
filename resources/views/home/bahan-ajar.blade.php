0f02087ee2a1c8e8480be1df12f74e99b3b7d237
bintangjtobing <hello@bintangtobing.com>
2022-09-14 14:48:52 +0700

    running ads

--- resources/views/home/bahan-ajar.blade.php
+++ resources/views/home/bahan-ajar.blade.php
@@ -97,11 +97,6 @@
                             </ul>
 
                         </div>
-                        <div class="widget widget_links clearfix">
-
-                            <img src="{{asset('webpage/ads/ads-ex.jpg')}}" alt="" class="img-fluid">
-
-                        </div>
                         <div class="widget clearfix">
                             <iframe
                                 src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fboxity-central-indonesia&amp;width=240&amp;height=290&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true&amp;appId=499481203443583"
